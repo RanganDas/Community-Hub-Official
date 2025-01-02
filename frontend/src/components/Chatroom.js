3eecb3840728a8bda3dc475da0d90fda865852f3
Rangan Das <87899366+RanganDas@users.noreply.github.com>
2025-01-02 20:56:39 +0530

    Updated Chatroom.js

--- frontend/src/components/Chatroom.js
+++ frontend/src/components/Chatroom.js
@@ -32,7 +32,7 @@ const Chatroom = () => {
   const [loading, setLoading] = useState(false);
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [socket, setSocket] = useState(null);
-  const URL = "http://localhost:5000";
+  const URL = "https://community-hub-official.onrender.com";
 
   const fetchUserId = async () => {
     const token = localStorage.getItem("token");
