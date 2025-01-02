3a3ad972931a1bbc4cd58a22b4e255695305027f
Rangan Das <87899366+RanganDas@users.noreply.github.com>
2025-01-02 20:59:45 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/api/login", {
+      const response = await axios.post("https://community-hub-official.onrender.com/api/login", {
         email,
         password,
       });
