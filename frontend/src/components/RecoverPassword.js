b518f54704016afe6628b73cd4ce81cfa46244c6
RanganDas <rangan.das111@gmail.com>
2025-01-10 12:58:20 +0530

    Error Fixes

--- frontend/src/components/RecoverPassword.js
+++ frontend/src/components/RecoverPassword.js
@@ -1,61 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 const RecoverPassword = () => {
-  const [email, setEmail] = useState("");
-  const navigate = useNavigate();
+    return (
+        <div>
+            <h1>Recover Password</h1>
+        </div>
+    )
+}
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "https://community-hub-official.onrender.com/api/forgotpassword",
-        { email }
-      );
-      if (response.status === 200) {
-        toast.success("OTP sent to your email.");
-        setTimeout(() => {
-          navigate("/verify-password"); // Navigate to OTP verification page
-        }, 4000);
-      }
-    } catch (error) {
-      toast.error("Error sending OTP. Please check your email.");
-    }
-  };
-
-  return (
-    <div className="recovery-password-container">
-      <form onSubmit={handleSubmit} className="recovery-password-form">
-        <h2>Enter the email linked to your account</h2>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          className="input-field"
-        />
-        <button type="submit" style={{ background: "green" }}>
-          Send OTP
-        </button>
-      </form>
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </div>
-  );
-};
-
-export default RecoverPassword;
+export default RecoverPassword
